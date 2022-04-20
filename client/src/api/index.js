7e5d9f6228a01a8faf7c0c6615d13cd867528d26
Blake Han <byh2003@nyu.edu>
2022-04-19 22:09:46 -0400

    update function, beautify the site

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const url = process.env.NODE_ENV === 'PRODUCTION' ? 'https://eczema-forum.herokuapp.com/diarys' : 'http://localhost:5000/diarys'
-
+// const url = process.env.NODE_ENV === 'PRODUCTION' ? 'https://eczema-forum.herokuapp.com/diarys' : 'http://localhost:5000/diarys'
+const url = 'https://eczema-forum.herokuapp.com/diarys'
 export const fetchDiarys = ()=> axios.get(url);
 export const createDiary = (newDiary) =>axios.post(url, newDiary);
 export const updateDiary = (id, updatedDiary) => axios.patch(`${url}/${id}`, updatedDiary);
